85f9e6b614aa0dada71383467ddbb6f443454c4d
chrs.ptr.hntr <horaldo_hunter@hotmail.com>
2019-09-03 22:44:29 +0100

    Synchronised dropdown filter options and started navbar

--- apps/attendance.py
+++ apps/attendance.py
@@ -11,10 +11,19 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 df = pd.read_csv('https://raw.githubusercontent.com/chrs-ptr-hntr/AttainmentSuite/master/data/attendance.csv')
 
 df["ACORN"] = df["ACORN"].astype('str')
+df["SIMD"] = df["SIMD"].astype('str')
 
 app.layout = html.Div([
         
-    dbc.Row(dbc.Col(html.H1("The Attainment Suite"))),
+    dbc.Row(dbc.Col(dbc.Navbar(
+                
+            dbc.Col(html.Img(src="https://raw.githubusercontent.com/chrs-ptr-hntr/AttainmentSuite/master/assets/logo.jpg", height="50px")),
+                
+               color="dark",
+               dark=True,
+            
+            )
+    )),
         
     dbc.Row([dbc.Col([dcc.Dropdown(
                 id='school_filter',
@@ -29,10 +38,16 @@ app.layout = html.Div([
                 multi = True),
                         
                 dcc.Dropdown(
+                id='simd_filter',
+                options=[{'label': i, 'value': i} for i in sorted(df["SIMD"].unique())],
+                placeholder="Select SIMD Quintile",
+                multi = True),
+                        
+                dcc.Dropdown(
                 id='gender_filter',
                 options=[{'label': i, 'value': i} for i in sorted(df["Gender"].unique())],
                 placeholder="Select Gender",
-                multi = True)]),
+                multi = True)], width = 3),
             
             dbc.Col(dcc.Graph(id='timeseriesgraph'))
 
@@ -41,12 +56,17 @@ app.layout = html.Div([
                     ])
 
 @app.callback(
-    Output('timeseriesgraph', 'figure'),
+    [Output('timeseriesgraph', 'figure'),
+     Output('acorn_filter', 'options'),
+     Output('school_filter', 'options'),
+     Output('gender_filter', 'options'),
+     Output('simd_filter', 'options')],
     [Input('acorn_filter', 'value'),
      Input('school_filter', 'value'),
-     Input('gender_filter', 'value')])
+     Input('gender_filter', 'value'),
+     Input('simd_filter', 'value')])
     
-def update_figure(selected_acorn, selected_school, selected_gender):
+def update_figure(selected_acorn, selected_school, selected_gender, selected_simd):
     
     if not selected_acorn:
         dfa = df
@@ -62,10 +82,15 @@ def update_figure(selected_acorn, selected_school, selected_gender):
         dfa = dfa
     else:
         dfa = dfa[dfa['Gender'].isin(selected_gender)]
+        
+    if not selected_simd:
+        dfa = dfa
+    else:
+        dfa = dfa[dfa['SIMD'].isin(selected_simd)]
     
     dff = dfa.groupby("Year", as_index = False, sort = "ascending").sum()
     
-    return {
+    return [{
             'data': [
                 {'x': dff.Year, 'y':dff.Actual/dff.Possible, 'mode': 'lines'}
             ], 
@@ -73,11 +98,17 @@ def update_figure(selected_acorn, selected_school, selected_gender):
                 'title': 'Attendance over time',
                 'yaxis': dict(range=[0,1],tickformat="%"),
                 'xaxis': {'type': 'category', 'categoryorder': 'category ascending'},
-                'margin':{'pad':20},
+                'margin':{'pad':20}
             }
         
-    }
-
+        },
+            
+        [{'label': i, 'value': i} for i in sorted(dfa["ACORN"].unique(), reverse=True)],
+        [{'label': i, 'value': i} for i in sorted(dfa["School"].unique())],
+        [{'label': i, 'value': i} for i in sorted(dfa["Gender"].unique())],
+        [{'label': i, 'value': i} for i in sorted(dfa["SIMD"].unique())]
+        
+]
 
 
 if __name__ == '__main__':
