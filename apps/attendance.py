3ce3a8d049ca16245986c1aac202d844f9375506
chrs.ptr.hntr <horaldo_hunter@hotmail.com>
2019-09-08 07:25:37 +0100

    Navbar changes, additional year slicer

--- apps/attendance.py
+++ apps/attendance.py
@@ -12,25 +12,36 @@ df = pd.read_csv('https://raw.githubusercontent.com/chrs-ptr-hntr/AttainmentSuit
 
 df["ACORN"] = df["ACORN"].astype('str')
 df["SIMD"] = df["SIMD"].astype('str')
+df["Year"] = df["Year"].astype('str')
+
 
 app.layout = html.Div([
         
-   dbc.Navbar(dbc.Row([
-                
-            dbc.Col(html.Img(src="https://raw.githubusercontent.com/chrs-ptr-hntr/AttainmentSuite/master/assets/mainlogo.jpg", height="50px")),
-            
-            ]),
-            
-                
-               color="dark",
-               dark=True,
-               sticky=True
-            
-            ),
+        dbc.Navbar(dbc.Row(
+                [
+                    dbc.Col(html.Img(src="https://raw.githubusercontent.com/chrs-ptr-hntr/AttainmentSuite/master/assets/mainlogo.jpg", height="50px"),width=2),
+                    dbc.Col(dbc.NavbarBrand("The Attainment Suite"), width = 6, align="center"),
+                    dbc.Col(
+                    dbc.DropdownMenu(
+                            children=[
+                            dbc.DropdownMenuItem("More tools", header=True),
+                            dbc.DropdownMenuItem("SNSA", href="#"),
+                            dbc.DropdownMenuItem("CfE Levels", href="#"),
+                        ],
+                        nav=True,
+                        in_navbar=True,
+                        label="More tools",
+        ), width=4, align='center'),
+
+                ]),
+    color="dark",
+    dark=True,
+    sticky=True
+),
         
        dbc.Row([dbc.Col([
                 
-                html.H3("Filters", style={'align': 'center'}),
+                html.H3("Filters",style={'color': 'navyblue'}),
                 
                 dcc.Dropdown(
                 id='school_filter',
@@ -54,13 +65,19 @@ app.layout = html.Div([
                 id='gender_filter',
                 options=[{'label': i, 'value': i} for i in sorted(df["Gender"].unique())],
                 placeholder="Select Gender",
-                multi = True)], width = 3),
+                multi = True),
+                        
+                dcc.Dropdown(
+                id='year_filter',
+                options=[{'label': i, 'value': i} for i in sorted(df["Year"].unique())],
+                placeholder="Select Year",
+                multi = True)], width = 3, style={'background-color':'Gainsboro'}),
             
-            dbc.Col(dcc.Graph(id='timeseriesgraph')),
+            dbc.Col(dcc.Graph(id='timeseriesgraph'),width = 4),
 
-            dbc.Col(dcc.Graph(id='bubblegraph'))
+            dbc.Col(dcc.Graph(id='bubblegraph'),width = 4)
 
-             ]),
+             ])
 
                     ])
 
@@ -70,45 +87,70 @@ app.layout = html.Div([
      Output('acorn_filter', 'options'),
      Output('school_filter', 'options'),
      Output('gender_filter', 'options'),
-     Output('simd_filter', 'options')],
+     Output('simd_filter', 'options'),
+     Output('year_filter', 'options')],
     [Input('acorn_filter', 'value'),
      Input('school_filter', 'value'),
      Input('gender_filter', 'value'),
-     Input('simd_filter', 'value')])
+     Input('simd_filter', 'value'),
+     Input('year_filter', 'value')])
     
-def update_figure(selected_acorn, selected_school, selected_gender, selected_simd):
+def update_figure(selected_acorn, selected_school, selected_gender, selected_simd, selected_year):
     
     if not selected_acorn:
         dfa = df
-        dfp = df       
+        dfp = df
+        afa = df
+        afp = df
     else:
         dfa = df[df['ACORN'].isin(selected_acorn)]
         dfp = df[df['ACORN'].isin(selected_acorn)]
+        afa = df
+        afp = df
                 
     if not selected_school:
-        dfa = dfa      
+        dfa = dfa
+        afa = afa
     else:
         dfa = dfa[dfa['School'].isin(selected_school)]
-        
+        afa = afa[afa['School'].isin(selected_school)]   
+    
     if not selected_gender:
         dfa = dfa
-        dfp = dfp        
+        dfp = dfp  
+        afa = afa
+        afp = afp
+        
     else:
         dfa = dfa[dfa['Gender'].isin(selected_gender)]
         dfp = dfp[dfp['Gender'].isin(selected_gender)]
+        afa = afa[afa['Gender'].isin(selected_gender)]
+        afp = afp[afp['Gender'].isin(selected_gender)]       
+        
               
     if not selected_simd:
         dfa = dfa
         dfp = dfp
+        afa = afa
+        afp = afp
     else:
         dfa = dfa[dfa['SIMD'].isin(selected_simd)]
         dfp = dfp[dfp['SIMD'].isin(selected_simd)]
+        afa = afa[afa['SIMD'].isin(selected_simd)]
+        afp = afp[afp['SIMD'].isin(selected_simd)]
+        
+    if not selected_year:
+        afa = afa
+        afp = afp
+    else:
+        afa = afa[afa['Year'].isin(selected_year)]
+        afp = afp[afp['Year'].isin(selected_year)] 
     
     dff = dfa.groupby("Year", as_index = False, sort = "ascending").sum()
     dfpkc = dfp.groupby("Year", as_index = False, sort = "ascending").sum()
     
-    aff = dfa.groupby("ACORN", as_index = False, sort = "ascending").sum()
-    afpkc = dfp.groupby("ACORN", as_index = False, sort = "ascending").sum()
+    aff = afa.groupby("ACORN", as_index = False, sort = "ascending").sum()
+    afpkc = afp.groupby("ACORN", as_index = False, sort = "ascending").sum()
     
     return [{
             'data': [
@@ -146,8 +188,8 @@ def update_figure(selected_acorn, selected_school, selected_gender, selected_sim
         [{'label': i, 'value': i} for i in sorted(dfa["ACORN"].unique(), reverse=True)],
         [{'label': i, 'value': i} for i in sorted(dfa["School"].unique())],
         [{'label': i, 'value': i} for i in sorted(dfa["Gender"].unique())],
-        [{'label': i, 'value': i} for i in sorted(dfa["SIMD"].unique())]
-        
+        [{'label': i, 'value': i} for i in sorted(dfa["SIMD"].unique())],
+        [{'label': i, 'value': i} for i in sorted(afa["Year"].unique())]
 ]
 
 
