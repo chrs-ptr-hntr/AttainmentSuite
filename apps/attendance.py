65a5d16107d519e7fc8ea95b548671f53ba2581c
chrs.ptr.hntr <horaldo_hunter@hotmail.com>
2019-09-08 20:27:47 +0100

    bubble sizes fixed

--- apps/attendance.py
+++ apps/attendance.py
@@ -42,7 +42,7 @@ app.layout = html.Div([
         
        dbc.Row([dbc.Col([
                 
-                html.H4("Filters",style={'color': 'darkblue', 'text-align':'center'}),
+                html.H4("Filters",style={'color': 'steelblue', 'text-align':'center'}),
                 
                 dcc.Dropdown(
                 id='school_filter',
@@ -74,13 +74,13 @@ app.layout = html.Div([
                 placeholder="Select Year",
                 multi = True)], width = 3),
             
-            dbc.Col(dcc.Graph(id='timeseriesgraph'),width = 5,style={'background-color':'Gainsboro'}),
+            dbc.Col(dcc.Graph(id='timeseriesgraph'),width = 5),
 
-            dbc.Col(dcc.Graph(id='bubblegraph'),width = 4,style={'background-color':'Gainsboro'})
+            dbc.Col(dcc.Graph(id='bubblegraph'),width = 4)
 
              ])
 
-                    ])
+                    ],style={'background-color':'WhiteSmoke'})
 
 @app.callback(
     [Output('timeseriesgraph', 'figure'),
@@ -155,7 +155,7 @@ def update_figure(selected_acorn, selected_school, selected_gender, selected_sim
     
     bubblesize = [len(afa[afa['ACORN'] == '5'])/len(afa),len(afa[afa['ACORN'] == '4'])/len(afa),len(afa[afa['ACORN'] == '3'])/len(afa),len(afa[afa['ACORN'] == '2'])/len(afa),len(afa[afa['ACORN'] == '1'])/len(afa)]
     pkcbubblesize = [len(afp[afp['ACORN'] == '5'])/len(afp),len(afp[afp['ACORN'] == '4'])/len(afp),len(afp[afp['ACORN'] == '3'])/len(afp),len(afp[afp['ACORN'] == '2'])/len(afp),len(afp[afp['ACORN'] == '1'])/len(afp)]
-   
+    
     return [{
             'data': [
                 
@@ -175,8 +175,8 @@ def update_figure(selected_acorn, selected_school, selected_gender, selected_sim
         {
             'data': [
                 
-                {'x': afpkc.ACORN, 'y':afpkc.Actual/afpkc.Possible, 'mode': 'markers','size':pkcbubblesize, 'name': "PKC", 'marker':{'color':'darkgrey'}},
-                {'x': aff.ACORN, 'y':aff.Actual/aff.Possible, 'mode': 'markers','size':bubblesize,'name': "Selection",'marker':{'color':'blue'}},
+                {'x': afpkc.ACORN, 'y':afpkc.Actual/afpkc.Possible, 'mode': 'markers','name': "PKC", 'marker':{'color':'darkgrey','size':pkcbubblesize, 'sizemode':'area','sizeref':2.*max(pkcbubblesize)/(40**2), 'sizemin':4}},
+                {'x': aff.ACORN, 'y':aff.Actual/aff.Possible, 'mode': 'markers', 'name': "Selection",'marker':{'color':'blue','size':bubblesize,'sizemode':'area','sizeref':2.*max(bubblesize)/(40**2),'sizemin':4}},
                 
             ], 
             'layout': {
